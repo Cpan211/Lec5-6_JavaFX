f864ed609a756c8c9a67914a395d95b4b3590b6e
cpan211 <cpan211fall2023@gmail.com>
2023-10-04 06:50:52 -0400

    Moved Anonymous Class to its own Package

--- src/EventHandler/AnonymousClass/MouseClick.java
+++ src/EventHandler/AnonymousClass/MouseClick.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
-package EventHandler.InnerClass.AnonymousClass;
+package EventHandler.AnonymousClass;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
